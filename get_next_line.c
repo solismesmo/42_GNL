fe227ad0b94f5adc905e472017bc183fa2703f1c
Lis Costa <livieira@student.42sp.org.br>
2023-08-25 06:28:41 -0300

    Feat: 1st escope gnl.

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,23 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 03:03:13 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/25 03:57:28 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/25 04:59:25 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char		*rest;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	rest = ft_read(fd, rest);
+	if (!rest)
+		return (NULL);
+	line = ft_getline(rest);
+	rest = ft_get_rest(rest);
+	return (line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 03:00:51 by livieira          #+#    #+#             */
-/*   Updated: 2023/08/25 04:00:51 by livieira         ###   ########.fr       */
+/*   Updated: 2023/08/25 04:45:12 by livieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdio.h>
 # include <unistd.h>
