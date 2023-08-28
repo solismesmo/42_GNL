b9ed6c10c54cc1f97de6cb52758f0cbda4103d0c
Lis Costa <livieira@student.42sp.org.br>
2023-08-28 08:47:30 -0300

    Feat: added 3 files (*.c and .h) for bonus part.

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: livieira <livieira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 08:43:09 by livieira          #+#    #+#             */
+/*   Updated: 2023/08/28 08:46:45 by livieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+#endif
